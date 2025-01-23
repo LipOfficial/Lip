64df38341d2505f25d02ab1d8f74265e14312de3
Pd <69450129+Pd233@users.noreply.github.com>
2025-01-23 13:03:03 +0800

    feat: Lip.Context implementation

---
+++ Lip.Context/Implementation/GitImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace Lip.Context.Implementation;
+internal class GitImpl : IGit
+{
+    public async Task Clone(string repository, string directory, string? branch = null, int? depth = null)
+    {
+        await Task.Run(() =>
+        {
+            var cloneOptions = new CloneOptions(new FetchOptions { Depth = depth ?? 1 })
+            {
+                BranchName = branch,
+            };
+            Repository.Clone(repository, directory, cloneOptions);
+        });
+    }
+}
